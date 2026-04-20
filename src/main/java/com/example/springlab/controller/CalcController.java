a42c7a055cae4f60510f8f7527b5a0a5cbfbaf4a
yunji cho <rlarn2003@naver.com>
2026-04-20 18:58:10 +0900

    first commit

---
+++ src/main/java/com/example/springlab/controller/CalcController.java
@@ -0,0 +1,27 @@
+package com.example.springlab.controller;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.swing.*;
+
+@Controller
+public class CalcController {
+    @GetMapping(value = "/calc")
+    public String calc(int num1, int num2, String oper,Model model){
+
+        if(oper.equals("/")&&num2==0){
+            model.addAttribute("errorMsg", "나눗셈 연산시 두 번째 숫자는 0일 수 없습니다!!");
+            return "errorResult";
+        }
+
+        int result = 0;
+        if(oper.equals("+")) result = num1 + num2;
+        else if(oper.equals("-")) result = num1-num2;
+        else if(oper.equals("/")) result = num1/num2;
+        else if(oper.equals("*")) result = num1*num2;
+        model.addAttribute("result",result);
+        return "calcResult";
+    }
+}
